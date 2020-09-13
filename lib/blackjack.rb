f50697a03f0aac64c003639ffb8461bfdf1f50e5
Elias Delgado <evdelgado99@gmail.com>
2020-09-13 06:00:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def welcome
   # code #welcome here
   puts "Welcome to the Blackjack Table"
@@ -51,7 +53,8 @@ def hit?(sum)
   if 's'
     nil
   elsif 'h'
-    return deal_card + sum
+    deal_card
+    binding.pry
   else
     puts invalid_command
     puts prompt_user
