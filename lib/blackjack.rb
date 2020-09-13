c80b70fcc105d120bed630442a768481d55c40d2
Elias Delgado <evdelgado99@gmail.com>
2020-09-13 05:44:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,7 @@ def hit?(current_card_total)
   elsif 'h'
     deal_card
   else
-    puts invalid_command
+    puts "Please enter a valid command"
     puts prompt_user
     puts get_user_input
   end
