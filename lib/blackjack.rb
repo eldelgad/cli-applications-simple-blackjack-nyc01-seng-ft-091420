f7179c28c39d7d846d781c8531ebb884eb33b448
Elias Delgado <evdelgado99@gmail.com>
2020-09-13 06:23:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,12 +33,9 @@ end
 def initial_round
   # code #initial_round here
   
-  deal_card
-  deal_card
-  
   sum = deal_card + deal_card
   
-  p display_card_total(sum)
+  return display_card_total(sum)
   
   return sum
   
