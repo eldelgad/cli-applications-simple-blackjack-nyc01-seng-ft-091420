7dd14e7fbe1b1638c8dc30c91942e82081d66f0d
Elias Delgado <evdelgado99@gmail.com>
2020-09-13 06:29:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,9 +47,9 @@ def hit?(card_total)
   prompt_user
   get_user_input
   
-  if user_input == "h"
+  if get_user_input == "h"
     card_total += deal_card
-  elsif user_input == "s"
+  elsif get_user_input == "s"
     card_total
   else
     invalid_command
